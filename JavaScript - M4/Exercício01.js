a68ee185d403024c570e50323c90d5a2c7258e0b
Luiz Gustavo <42076223+Guuh86@users.noreply.github.com>
2019-08-19 19:47:01 -0300

    Add files via upload

---
+++ JavaScript - M4/Exercício01.js
@@ -0,0 +1,23 @@
+//01
+let arrayNum = [1,2,3,4,5,6,7,8,9,10];
+
+//02
+for(i = 1; i < arrayNum.length; i++){
+    if((arrayNum[i] % 2) != 0){
+        console.log(i);
+    }
+}
+
+//03
+let arrayDobro = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+//04
+
+//05
+for(x = 1; x < arrayDobro.length; x++){
+    if((arrayDobro[x]) % 2 == 0){
+        console.log(x)
+    }
+}
+
+//06
